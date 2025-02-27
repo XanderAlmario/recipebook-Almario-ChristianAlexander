e4fd7bae2a77621dae0442e6fa2f8ae22ee0004b
XanderAlmario <christian.almario@student.ateneo.edu>
2025-02-27 15:18:57 +0800

    Updated Ingredient and Recipe models in models.py
    Updated the Ingredient and Recipe models in models.py, setting the string representation and absolute urls of each model. In order to do this, reverse was imported from django.urls for each model's get_absuolute_url method.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Ingredient(models.Model):
   name = models.CharField(max_length=100)
 
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('ingredient', args=[str(self.name)])
+
 class Recipe(models.Model):
   name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('recipe', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
   quantity = models.IntegerField()
