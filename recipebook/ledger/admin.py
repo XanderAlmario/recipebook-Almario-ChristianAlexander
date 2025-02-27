238635db3df7cc51b2d1185cd6715c290bc379bc
XanderAlmario <christian.almario@student.ateneo.edu>
2025-02-27 23:49:47 +0800

    Updated admin.py
    Created the admin panel for Recipe and an Inline Admin for RecipeIngredient

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Recipe, RecipeIngredient
 # Register your models here.
+
+class RecipeIngredientInLine(admin.TabularInline):
+  model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+  inlines = [RecipeIngredientInLine,]
+
+admin.site.register(Recipe, RecipeAdmin)
