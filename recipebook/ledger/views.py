f504c2f7b55385a79512b1249f672bc589d92506
XanderAlmario <christian.almario@student.ateneo.edu>
2025-02-27 23:38:29 +0800

    Updated views.py and added DetailView

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_detail.html'
