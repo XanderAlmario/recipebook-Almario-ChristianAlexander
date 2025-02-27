1ac01e363c44c25b1fbcb866e5244c1f9c82707c
XanderAlmario <christian.almario@student.ateneo.edu>
2025-02-27 23:34:36 +0800

    Updated get_absolute_url
    Fixed formatting

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
     return self.name
 
   def get_absolute_url(self):
-    return reverse('ingredient', args=[str(self.name)])
+    return reverse('ledger:ingredient', args=[str(self.name)])
 
 class Recipe(models.Model):
   name = models.CharField(max_length=100)
@@ -18,18 +18,18 @@ class Recipe(models.Model):
     return self.name
 
   def get_absolute_url(self):
-    return reverse('recipe', args=[str(self.name)])
+    return reverse('ledger:recipe', args=[str(self.pk)])
 
 class RecipeIngredient(models.Model):
   quantity = models.CharField(max_length=100)
   ingredient = models.ForeignKey(
     Ingredient,
     on_delete=models.CASCADE,
-    related_name='ingredient'
+    related_name='recipe'
   )
   recipe = models.ForeignKey(
     Recipe,
     on_delete=models.CASCADE,
-    related_name='recipe'
+    related_name='ingredient'
   )
   
