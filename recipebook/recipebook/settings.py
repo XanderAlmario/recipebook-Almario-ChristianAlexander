2d25157815911d66a7b199e5b4f9145dfc6e3436
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-10 22:55:06 +0800

    Added backend for password reset

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -130,3 +130,4 @@ STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
