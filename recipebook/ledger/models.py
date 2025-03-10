9b8dac8626fe41ccf366ac357c190846936b2563
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-10 21:11:29 +0800

    Removed Profile model from ledger app

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -36,8 +36,3 @@ class RecipeIngredient(models.Model):
     on_delete=models.CASCADE,
     related_name='ingredients'
   )
-  
-class Profile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  name = models.CharField(max_length=50)
-  bio = models.TextField()
