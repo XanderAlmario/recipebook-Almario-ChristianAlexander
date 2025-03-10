ef141bffa8e1be218742b30ca513fffda097519d
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-10 18:24:58 +0800

    Created Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Ingredient(models.Model):
@@ -33,3 +34,7 @@ class RecipeIngredient(models.Model):
     related_name='ingredients'
   )
   
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=50)
+  bio = models.TextField()
