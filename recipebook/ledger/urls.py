d514dd130e22f9d747c74bf01d767972b30c475c
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-25 00:15:55 +0800

    Added path for RecipeImageCreativeView

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from .views import (
-    RecipeListView, RecipeDetailView, RecipeCreateView
+    RecipeListView, RecipeDetailView, RecipeCreateView, RecipeImageCreateView
 )
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
-    path('recipe/add', RecipeCreateView.as_view(), name='recipe-add')
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe-add'),
+    path(
+        'recipe/<int:pk>/add_image', 
+        RecipeImageCreateView.as_view(), 
+        name='recipe-image-add'
+    )
 ]
 
 app_name = "ledger"
