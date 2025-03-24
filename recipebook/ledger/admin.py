0046c57e90ea2ae9765e6aecb761d45f61634f21
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-24 15:10:45 +0800

    Added RecipeImage to the admin page of recipe

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, RecipeImage
 # Register your models here.
 
 
 class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInLine(admin.TabularInline):
+    model = RecipeImage
 
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInLine,]
+    inlines = [RecipeIngredientInLine, RecipeImageInLine,]
 
 
 admin.site.register(Recipe, RecipeAdmin)
