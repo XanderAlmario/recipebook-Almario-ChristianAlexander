5a38ee707a5d4784af1495b19d6b684db02b7257
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-24 21:11:44 +0800

    Added a path for RecipeCreateView

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
-
+from .views import (
+    RecipeListView, RecipeDetailView, RecipeCreateView
+)
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe-list'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe')
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe-add')
 ]
 
 app_name = "ledger"
