b31fd0582cb709539da0eb0292a42dc88ff30d3b
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-25 00:16:20 +0800

    Create RecipeImageCreateView for users to upload images to recipes

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,10 +1,12 @@
-from .models import Recipe
-from .forms import RecipeForm
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 class RecipeListView(ListView):
@@ -23,3 +25,39 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     form_class = RecipeForm
     template_name = 'recipe_add.html'
     redirect_field_name = 'registration/login.html'
+
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = 'recipe_image_add.html'
+    redirect_field_name = 'registration/login.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+        ctx['pk'] = pk
+        ctx['recipe'] = Recipe.objects.get(pk=pk)
+        ctx['form'] = RecipeImageForm()
+        return ctx
+
+    def get_success_url(self):
+        return reverse("ledger:recipe", kwargs={"pk": self.kwargs["pk"]})
+
+    def post(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        form = RecipeImageForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            r = RecipeImage()
+            r.image = request.FILES.get('image')
+            r.description = request.POST.get('description')
+            r.recipe = Recipe.objects.get(pk=pk)
+            r.save()
+
+            return redirect(reverse('ledger:recipe', args=[pk]))
+        else:
+            self.object_list = self.get_queryset(**kwargs)
+            context = self.get_context_data(**kwargs)
+            context['form'] = form
+            return self.render_to_response(context)
