a49691691af3a2b60b9112f636363223f27c2fcc
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-24 18:14:31 +0800

    Fixed related name for recipe foreign key in RecipeImage

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -48,5 +48,5 @@ class RecipeImage(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='recipe'
+        related_name='images'
     )
