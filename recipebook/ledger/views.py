9104acef4616216a7796f8ed6e9ad10156820157
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-24 21:12:20 +0800

    Fixed RecipeCreateView
    Also made it so that it needs a logged in user to access

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,10 +1,10 @@
 from .models import Recipe
+from .forms import RecipeForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
-
-# Create your views here.
+from django.shortcuts import render
 
 
 class RecipeListView(ListView):
@@ -18,7 +18,8 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     redirect_field_name = 'registration/login.html'
 
 
-class RecipeCreateView(CreateView):
+class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
-    form_class = TaskFrom
-    template_name = 'firstapp/task_detail.html'
+    form_class = RecipeForm
+    template_name = 'recipe_add.html'
+    redirect_field_name = 'registration/login.html'
